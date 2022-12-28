32ef77e1bf3c9e5c5010bdcd6108db3ed5319ca9
waonderboy <crazy7523@gmail.com>
2022-12-28 19:26:40 +0900

    #22 - 공통응답 Exception
    비즈니스 로직에서 예측가능한 예외는 모두 BaseException으로 throw한다. 이는 ControllerAdvice에서 일괄적으로 처리한다.

---
+++ src/main/java/com/brogs/crm/common/exception/InvalidParamException.java
@@ -0,0 +1,23 @@
+package com.brogs.crm.common.exception;
+
+
+import com.brogs.crm.common.response.ErrorCode;
+
+public class InvalidParamException extends BaseException {
+
+    public InvalidParamException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public InvalidParamException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public InvalidParamException(String errorMsg) {
+        super(errorMsg, ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public InvalidParamException(String message, ErrorCode errorCode) {
+        super(message, errorCode);
+    }
+}
