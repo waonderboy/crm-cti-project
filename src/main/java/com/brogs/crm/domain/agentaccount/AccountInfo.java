c0bf8617b8364f582bea296ad189ca611b536623
waonderboy <crazy7523@gmail.com>
2022-12-30 20:27:15 +0900

    #30 - 기능에 맞는 계정, 프로필 DTO 개발

--- src/main/java/com/brogs/crm/domain/agentaccount/AccountInfo.java
+++ src/main/java/com/brogs/crm/domain/agentaccount/AccountInfo.java
@@ -1,5 +1,7 @@
 package com.brogs.crm.domain.agentaccount;
 
+import com.brogs.crm.common.security.jwt.JwtTokens;
+import com.brogs.crm.domain.agentaccount.agentprofile.AgentProfile;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -19,6 +21,8 @@ public class AccountInfo {
         private String password;
         private String extension;
         private Set<String> authorities;
+        private boolean hasProfile;
+        private AgentInfo agentInfo;
 
         public static Main from(AgentAccount entity) {
             return  Main.builder()
@@ -26,6 +30,17 @@ public class AccountInfo {
                     .password(entity.getPassword())
                     .extension(entity.getExtension())
                     .authorities(Set.of(entity.getRole().toString()))
+                    .hasProfile(entity.isHasProfile())
+                    .build();
+        }
+
+        public static Main from(AgentAccount account, AgentProfile profile) {
+            return  Main.builder()
+                    .identifier(account.getIdentifier())
+                    .extension(account.getExtension())
+                    .authorities(Set.of(account.getRole().toString()))
+                    .hasProfile(account.isHasProfile())
+                    .agentInfo(AgentInfo.from(profile))
                     .build();
         }
     }
@@ -38,11 +53,48 @@ public class AccountInfo {
         private String password;
         private String role;
 
-        public static AccountInfo.Register from(AgentAccount agentAccount) {
+        public static AccountInfo.Register from(AgentAccount entity) {
             return Register.builder()
-                    .identifier(agentAccount.getIdentifier())
-                    .password(agentAccount.getPassword())
-                    .role(agentAccount.getRole().toString())
+                    .identifier(entity.getIdentifier())
+                    .password(entity.getPassword())
+                    .role(entity.getRole().toString())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class Login {
+        private JwtTokens jwtTokens;
+        private boolean hasProfile;
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class AgentInfo {
+        private String name;
+        private String profileImage;
+        private String email;
+        private String phoneNumber;
+        private String address;
+        private String groupName;
+        private String confirmToken;
+        private int age;
+        private boolean activated;
+
+        public static AgentInfo from(AgentProfile entity) {
+            return AgentInfo.builder()
+                    .name(entity.getName())
+                    .profileImage(entity.getProfileImage())
+                    .email(entity.getEmail())
+                    .age(entity.getAge())
+                    .phoneNumber(entity.getPhoneNumber())
+                    .address(entity.getAddress())
+                    .groupName(entity.getGroupName())
+                    .confirmToken(entity.getConfirmToken())
+                    .activated(entity.isActivated())
                     .build();
         }
     }
--- src/main/java/com/brogs/crm/interfaces/controller/agentaccount/AccountDtoMapper.java
+++ src/main/java/com/brogs/crm/interfaces/controller/agentaccount/AccountDtoMapper.java
@@ -2,9 +2,8 @@ package com.brogs.crm.interfaces.controller.agentaccount;
 
 import com.brogs.crm.domain.agentaccount.AccountCommand;
 import com.brogs.crm.domain.agentaccount.AccountInfo;
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
+import com.brogs.crm.domain.externalmessenger.SendMethod;
+import org.mapstruct.*;
 
 @Mapper(
         componentModel = "spring",
@@ -12,9 +11,19 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.ERROR
 )
 public interface AccountDtoMapper {
-
+    // 회원가입 요청 & 응답
     AccountCommand.RegisterAccount of(AccountDto.SignUpReq request);
-    AccountCommand.login of(AccountDto.SignInReq request);
     AccountDto.SignUpRes of(AccountInfo.Register result);
+
+    // 로그인 요청
+    AccountCommand.login of(AccountDto.SignInReq request);
+
+    // 프로필 등록 요청 & 응답
+    @Mapping(target = "confirmMessageMethod", source = "confirmMessageMethod" )
+    AccountCommand.RegisterProfile of(AccountDto.ProfileReq request);
+    AccountDto.ProfileRes of(AccountInfo.AgentInfo result);
+
+    // 계정 정보요청
+    AccountDto.AccountInfoRes of(AccountInfo.Main result);
 
 }
