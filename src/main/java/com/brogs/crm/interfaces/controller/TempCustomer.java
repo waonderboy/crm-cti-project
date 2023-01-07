c826526cc9ecf5c429ba2fa49385a85f1d352bbe
waonderboy <crazy7523@gmail.com>
2023-01-07 15:01:45 +0900

    #43 - 임시 테스트를 위한 티켓, 고객 컨트롤러 및 고객 dto

---
+++ src/main/java/com/brogs/crm/interfaces/controller/TempCustomer.java
@@ -0,0 +1,13 @@
+package com.brogs.crm.interfaces.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TempCustomer {
+    private String snsId;
+    private String snsType;
+    private String email;
+    private String phoneNumber;
+}
